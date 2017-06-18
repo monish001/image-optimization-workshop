4857487347fd5c187e5316938e72d08fd0f2f710
Monish Gupta <monish.gupta1@gmail.com>
2017-06-19 01:07:01 +0530

    Added sizes attr

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
             alt="Brisbane lighthouse"
             srcSet={imgBrisbane_400_1x + ' 400w, ' + imgBrisbane_700_1x + ' 700w, ' + 
               imgBrisbane_400_2x + ' 800w, ' + imgBrisbane_700_2x + ' 1400w'}
-            />
+            sizes="(max-width: 700px) 100vw, 700px"/>
           <p className="app-intro">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
