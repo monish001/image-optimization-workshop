8e7e6ba03e79c3d7d0371fa55da3fb8ba6197c38
Monish Gupta <monish.gupta1@gmail.com>
2017-06-19 01:14:50 +0530

    Added 400_3x image as well for checking google page speed mobile is loading 3x dpi pic

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import './App.css';
 /**
  * Image sizes(px) to support
  * Desktop - 700, 1400
- * Mobile - 400, 800
+ * Mobile - 400, 800, 1200
  */
 let imgBrisbane_700_1x = require('./img/700w.jpg'); 
 let imgBrisbane_700_2x = require('./img/1400w.jpg'); 
 let imgBrisbane_400_1x = require('./img/400w.jpg'); 
 let imgBrisbane_400_2x = require('./img/800w.jpg'); 
+let imgBrisbane_400_3x = require('./img/1200w.jpg'); 
 
 class App extends Component {
   render() {
@@ -28,7 +29,7 @@ class App extends Component {
             src={imgBrisbane_700_1x}
             alt="Brisbane lighthouse"
             srcSet={imgBrisbane_400_1x + ' 400w, ' + imgBrisbane_700_1x + ' 700w, ' + 
-              imgBrisbane_400_2x + ' 800w, ' + imgBrisbane_700_2x + ' 1400w'}
+              imgBrisbane_400_2x + ' 800w, ' + imgBrisbane_400_3x + ' 1200w, ' + imgBrisbane_700_2x + ' 1400w'}
             sizes="(max-width: 700px) 100vw, 700px"/>
           <p className="app-intro">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
