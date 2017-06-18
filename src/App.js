fd8d84f74c3dd599ad6b4e839584edcaeb45e4a3
Monish Gupta <monish.gupta1@gmail.com>
2017-06-18 18:39:20 +0530

    Lets just have 1 image to start with.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import imgBrisbaneLighthouse from './img/brisbane-lighthouse.jpg';
-import imgQueenslandSpringbrook from './img/queensland-springbrook-national-park.jpg';
 import './App.css';
+
+/**
+ * Image sizes(px) to support
+ * Desktop - 700, 1400
+ * Mobile - 400, 800
+ */
+let imgBrisbaneLighthouse = require('./img/brisbane-lighthouse.jpg'); 
 
 class App extends Component {
   render() {
@@ -16,11 +21,11 @@ class App extends Component {
           <p className="app-intro">
             To get started, edit <code>src/App.js</code> and save to reload. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
-          <img src={imgBrisbaneLighthouse} alt=""/>
-          <p className="app-intro">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <img src={imgQueenslandSpringbrook} alt=""/>
+          <img 
+            src={imgBrisbaneLighthouse} 
+            alt="test"
+            srcSet=""
+            sizes=""/>
           <p className="app-intro">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
           </p>
